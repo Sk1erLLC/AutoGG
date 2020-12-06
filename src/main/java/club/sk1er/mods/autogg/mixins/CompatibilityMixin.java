c47e28c0cee0fac9a972b3c1afb8a0e3661907e6
Matthew Olsson <matthewcolsson@gmail.com>
2020-12-05 16:45:00 -0800

    Update to ModCore 2

---
+++ src/main/java/club/sk1er/mods/autogg/mixins/CompatibilityMixin.java
@@ -0,0 +1,12 @@
+package club.sk1er.mods.autogg.mixins;
+
+import net.minecraft.client.Minecraft;
+import org.spongepowered.asm.mixin.Mixin;
+
+/**
+ * Can be deleted as long as there is at least 1 other mixin. Otherwise building *will* fail
+ */
+@Mixin(Minecraft.class)
+public class CompatibilityMixin {
+
+}
